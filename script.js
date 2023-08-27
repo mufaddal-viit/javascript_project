3d6c0e92b0c85f7ed58bf5a63abb7ee8b7e7f8f6
PWCGLB\mcalcuttaw001 <mufaddal.c.calcuttawala@pwc.com>
2023-08-27 12:06:48 +0530

    frist commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// const prompt=require("prompt-sync")({sigint:true}); 
+//alert("hello welcome to the board")
+function h1Func() {
+    document.getElementById("h1").style.backgroundColor = "red"
+}
+
+function h1Func2() {
+    document.getElementById("h1").style.backgroundColor =""
+    document.getElementById("h1").style.color ="Black"
+    console.log('running')
+}
+
+const btn = document.getElementById('h1');
+let colors = ['red', 'blue', 'green', 'brown']
+let x = 0;
+btn.addEventListener('mouseover', function onClick() {
+    if (x == colors.length) x = 0;
+    btn.style.backgroundColor = colors[x]; 
+    btn.style.color = 'white';
+    console.log(x)
+    x = x+1
+});
+
+ele = document.getElementById("first")
+function myFunction(){
+    let x = prompt("enter password", "Mufaddal")
+    if(x == "muffu"){
+        ele.className = "second"
+        ele.innerText = " login successfull\nhere are your things from this website"
+    }
+    
+    
+
+}
+
