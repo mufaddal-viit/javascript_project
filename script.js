531d9aacb9c207cf02b301b6a8dc52776b4fdad9
PWCGLB\mcalcuttaw001 <mufaddal.c.calcuttawala@pwc.com>
2023-08-27 16:58:38 +0530

    evening time

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
-//gitpush 
+//gitpush
 //push third
 
-const h1s = document.getElementById('h1');
+const h1ss = document.getElementById("h1s");
 ele = document.getElementById("first div");
 btn = document.getElementById("btn");
+ele.className = "second";
 
-function h1Func() {
-    document.getElementById("h1").style.backgroundColor = "red"
-}
+
 function h1Func2() {
-    document.getElementById("h1").style.backgroundColor =""
-    document.getElementById("h1").style.color ="Black"
-    console.log('running')
+  document.getElementById("h1s").style.backgroundColor = "";
+  document.getElementById("h1s").style.color = "Black";
+  console.log("running");
 }
 
-let colors = ['red', 'blue', 'green', 'brown']
+let colors = ["red", "blue", "green", "brown"];
 let x = 0;
-h1s.addEventListener('mouseover', function onClick() {
-    if (x == colors.length) x = 0;
-    h1s.style.backgroundColor = colors[x]; 
-    h1s.style.color = 'white';
-    console.log(x)
-    x = x+1
+h1ss.addEventListener("mouseover", function onClick() {
+  if (x == colors.length) x = 0;
+  h1ss.style.backgroundColor = colors[x];
+  h1ss.style.color = "white";
+  console.log(x);
+  x = x + 1;
 });
-
-
-btn.addEventListener('click',function myFunction(){
-    let x = prompt("Enter Your Name ", "akka Bai")
-    if(x == "akka Bai" || x == "taha" ){
-        ele.className = "second"
-        ele.innerText = " login successfull\n All the best for you exams"
+// -------------------------------------
+let flag2 = 0;
+btn.addEventListener("click", function myFunction() {
+    if (flag2 == 0){
+    
+        let x = prompt("Enter Your Name ", "akka Bai");
+              if (x == "akka Bai" || x == "taha") {
+                ele.className = "firstly";
+                ele.innerText = " Login Successfull\n All The Best For Your Exams";
+                btn.style.color = "blue";
+                btn.innerHTML = "Restore";
+                console.log("second")
+                flag2 = 1; //turn it into abnormal state 
+            }
+        }
+    else {
+        console.log("here", flag2);
+        btn.style.color = "black";
+        btn.innerHTML = "Click for a Good Message";
+        // btn.style.font-style = ;?
+        ele.innerHTML = "Enter Correct Password";
+        ele.className = "second";
+        // alert("second time");
+        flag2 = 0;
     }
-    })
+    
+});
 
