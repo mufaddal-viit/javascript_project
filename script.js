e15d3b06dc3861ab6e861445e4120ca49057180c
PWCGLB\mcalcuttaw001 <mufaddal.c.calcuttawala@pwc.com>
2023-08-27 13:13:18 +0530

    first css commit

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 //gitpush 
+//push third
+
+const h1s = document.getElementById('h1');
+ele = document.getElementById("first div");
+btn = document.getElementById("btn");
+
 function h1Func() {
     document.getElementById("h1").style.backgroundColor = "red"
 }
-
 function h1Func2() {
     document.getElementById("h1").style.backgroundColor =""
     document.getElementById("h1").style.color ="Black"
     console.log('running')
 }
 
-const btn = document.getElementById('h1');
 let colors = ['red', 'blue', 'green', 'brown']
 let x = 0;
-btn.addEventListener('mouseover', function onClick() {
+h1s.addEventListener('mouseover', function onClick() {
     if (x == colors.length) x = 0;
-    btn.style.backgroundColor = colors[x]; 
-    btn.style.color = 'white';
+    h1s.style.backgroundColor = colors[x]; 
+    h1s.style.color = 'white';
     console.log(x)
     x = x+1
 });
 
-ele = document.getElementById("first")
-function myFunction(){
-    let x = prompt("enter password", "Mufaddal")
-    if(x == "muffu"){
-        ele.className = "second"
-        ele.innerText = " login successfull\nhere are your things from this website"
-    }
-    
-    
 
-}
+btn.addEventListener('click',function myFunction(){
+    let x = prompt("Enter Your Name ", "akka Bai")
+    if(x == "akka Bai" || x == "taha" ){
+        ele.className = "second"
+        ele.innerText = " login successfull\n All the best for you exams"
+    }
+    })
 
---
+++ trial.js
@@ -0,0 +1,11 @@
+let i = 0
+    for( let x = i; x < 4; x++){
+        console.log(i,x)
+        
+        // if (x == 4) x = 0;
+        // else {
+        //     console.log(i,x)
+        //     i = x+1;
+        //     break
+
+        }
